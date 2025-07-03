e04d577fe5ad97975de8b22aa4547c3d136903f9
valyo <582646+valyo@users.noreply.github.com>
2025-07-03 18:37:45 +0200

    fix formatting on all products page

--- sh_webshop/products.py
+++ sh_webshop/products.py
@@ -6,10 +6,10 @@ products = Blueprint("products", __name__)
 
 @products.route("/produkter")
 def index():
-    all_products = Product.query.order_by(Product.name).all()
+    all_products = Product.query.filter_by(is_active=True).order_by(Product.name).all()
     return render_template(
         "products/index.html",
         user=session.get("user"),
-        page_title="Produkter",
+        page_title="Alla produkter",
         products=all_products,
     )

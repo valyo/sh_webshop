477dbe8b556aa1fe2da20579223242b55233e2e8
valyo <582646+valyo@users.noreply.github.com>
2025-07-03 18:23:05 +0200

    display all products on produkter page

--- sh_webshop/products.py
+++ sh_webshop/products.py
@@ -1,10 +1,15 @@
 from flask import Blueprint, render_template, session
+from .models import Product
 
 products = Blueprint("products", __name__)
 
 
 @products.route("/produkter")
 def index():
+    all_products = Product.query.order_by(Product.name).all()
     return render_template(
-        "products/index.html", user=session.get("user"), page_title="Produkter"
+        "products/index.html",
+        user=session.get("user"),
+        page_title="Produkter",
+        products=all_products,
     )

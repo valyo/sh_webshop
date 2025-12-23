8872940756b933b7fdc8ff5a3568887bcd3bf9da
valyo <582646+valyo@users.noreply.github.com>
2025-12-23 18:44:33 +0100

    add individual products pages

--- sh_webshop/products.py
+++ sh_webshop/products.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, session
-from .models import Product
+from flask import Blueprint, render_template, session, abort
+from .models import Product, Category
 
 products = Blueprint("products", __name__)
 
@@ -12,4 +12,36 @@ def index():
         user=session.get("user"),
         page_title="Alla produkter",
         products=all_products,
+    )
+
+
+@products.route("/produkter/kategori/<slug>")
+def by_category(slug):
+    category = Category.query.filter_by(slug=slug).first()
+    if not category:
+        abort(404)
+    category_products = (
+        Product.query.filter_by(category_id=category.id, is_active=True)
+        .order_by(Product.name)
+        .all()
+    )
+    return render_template(
+        "products/index.html",
+        user=session.get("user"),
+        page_title=category.name,
+        products=category_products,
+        category=category,
+    )
+
+
+@products.route("/produkt/<slug>")
+def detail(slug):
+    product = Product.query.filter_by(slug=slug, is_active=True).first()
+    if not product:
+        abort(404)
+    return render_template(
+        "products/detail.html",
+        user=session.get("user"),
+        page_title=product.name,
+        product=product,
     )

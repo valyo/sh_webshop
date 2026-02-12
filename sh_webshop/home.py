065e060374087d1ce818c28f82717a6bdbafa5b0
valyo <582646+valyo@users.noreply.github.com>
2026-02-12 14:16:18 +0100

    add products showing on the home page

--- sh_webshop/home.py
+++ sh_webshop/home.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, session
+from .models import Product
 
 main = Blueprint("main", __name__)
 
 
 @main.route("/")
 def home():
-    return render_template("home.html", user=session.get("user"))
+    products = Product.query.filter_by(is_active=True).order_by(Product.name).all()
+    return render_template("home.html", user=session.get("user"), products=products)
